991b67b39b93f20d5c154c08013befb3f1b7c93a
vinizbjj <viniz.bjj@gmail.com>
2022-11-03 13:48:05 -0300

    Criação rota de autenticação do usuario

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,10 @@
+class SessionsController {
+    async create(request, response) {
+        const { email, password } = request.body;
+
+        return response.json({ email, password });
+    }
+}
+
+module.exports = SessionsController;
+
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
+const sessionsRouter = require("./sessions.routes");
 const notesRouter = require("./notes.routes");
 const tagsRouter = require("./tags.routes");
 
 const routes = Router();
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
 
