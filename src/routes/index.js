653852d563a7d4ee40dcc3425963bfdae03d4585
vinizbjj <viniz.bjj@gmail.com>
2022-11-07 17:31:07 -0300

    auth login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 
-const usersRouter = require("./users.routes");
 const sessionsRouter = require("./sessions.routes");
+const usersRouter = require("./users.routes");
 const notesRouter = require("./notes.routes");
 const tagsRouter = require("./tags.routes");
 
 const routes = Router();
-routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
+routes.use("/users", usersRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
 
