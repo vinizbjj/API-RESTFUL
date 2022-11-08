65751882af9243d2807cbad63a1732c38a5ca163
vinizbjj <viniz.bjj@gmail.com>
2022-11-08 14:38:43 -0300

    storage

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -3,14 +3,14 @@ const multer = require("multer");
 const crypto = require("crypto");
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
-const UPLOADS_FOLDER = path.resolve(__dirname, "uploads")
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads")
 
 const MULTER = {
     storage: multer.diskStorage({
         destination: TMP_FOLDER,
         filename(request, file, callback) {
             const fileHash = crypto.randomBytes(10).toString("hex");
-            const filename = `${fileHash}- ${file.originalname}`;
+            const filename = `${fileHash}-${file.originalname}`;
 
             return callback(null, filename);
         },
