0c2063570747454227316b469e977c6bd7f0eb3e
vinizbjj <viniz.bjj@gmail.com>
2022-11-09 00:26:52 -0300

    avatar imagem

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const path = require("path")
-const uploadConfig = require("../config/uploads")
+const uploadConfig = require("../configs/upload")
 
 class DiskStorage {
     async saveFile(file) {
